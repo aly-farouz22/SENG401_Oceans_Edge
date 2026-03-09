bad4ab758678f430d10df54b8c07e92a60e366a6
aly-farouz22 <aly.farouz@ucalgary.ca>
2026-03-09 14:28:06 -0600

    Add BoatUpgrade module to manage boat upgrades and effects

---
+++ client/src/game/objects/boat/BoatUpgrade.ts
@@ -0,0 +1,71 @@
+export interface Upgadre {
+    name: string;
+    description: string;
+    cost: number;
+    level: number;
+    levelMax: number;
+    effect: (boat: any) => void;
+}
+export default class BoatUpgrade {
+    private boat: any;
+    private upgrades: Upgadre[] = [];
+    constructor(boat: any) {
+        this.boat = boat;
+        this.initializeUpgrades();
+    }
+    // All upgrades are defined here
+    private initializeUpgrades() {
+        this.upgrades = [
+            {
+                name: "Stronger Net",
+                description: "Increase Catch Rate by 20%",
+                cost: 500,
+                level: 0,
+                levelMax: 3,
+                effect: (boat) => {boat.fishing.catchMultiplier = 1 + 0.2 * boat.upgrades.getLevel("Stronger Net")}
+            },
+            {
+                name: "Engine Boost",
+                description: "Increase Boat Speed by 15%",
+                cost: 1000,
+                level: 0,
+                levelMax: 3,
+                effect: (boat) => {boat.movement.catchMultiplier = 1 + 0.15 * boat.upgrades.getLevel("Engine Boost")}
+            },
+            {
+                name: "Eco Filter",
+                description: "Reduce Chance Of Catching Juvenile/Endangered Fish",
+                cost: 750,
+                level: 0,
+                levelMax: 2,
+                effect: (boat) => {boat.fishing.ecoFilterLevel = boat.upgrades.getLevel("Eco Filter")}
+            },
+        ];
+    }
+    purchaseUpgrade(name: string) {
+        const upgrade = this.upgrades.find(u => u.name === name);
+        if (!upgrade) return false;
+        if (upgrade.level > upgrade.levelMax) return false;
+        if (this.boat.money < upgrade.cost) return false;
+        this.boat.money -= upgrade.cost;
+        upgrade.level++;
+        upgrade.effect(this.boat);
+        console.log(`Upgrade Purchased: ${upgrade.name} (Level ${upgrade.level})`);
+        return true;
+    }
+    // This method return the current level of the upgrade
+    returnLevel(name: string) {
+        const upgrade = this.upgrades.find(u => u.name === name);
+        return upgrade ? upgrade.level : 0;
+    }
+    // This method displays upgrade info in  HUD
+    displayUpgrades() {
+        return this.upgrades.map( u => ({
+            name: u.name,
+            description: u.description,
+            cost: u.cost,
+            level: u.level,
+            maxLevel: u.levelMax
+        }));
+    } 
+}
