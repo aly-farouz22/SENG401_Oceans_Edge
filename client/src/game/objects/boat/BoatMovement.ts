78db27cc9f6c6228cef39cb650adb030d0cec6db
aly-farouz22 <aly.farouz@ucalgary.ca>
2026-03-12 22:22:29 -0600

    BoatMovement Improved to support WASD

--- client/src/game/objects/boat/BoatMovement.ts
+++ client/src/game/objects/boat/BoatMovement.ts
@@ -10,20 +10,38 @@ const SPEED = 200;
 export default class BoatMovement {
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   private sprite: Phaser.Physics.Arcade.Sprite;
+  private keys: { [key: string]: Phaser.Input.Keyboard.Key}
   public speedMultiplier = 1;
 
   constructor(scene: Phaser.Scene, sprite: Phaser.Physics.Arcade.Sprite) {
     this.sprite = sprite;
     this.cursors = scene.input.keyboard!.createCursorKeys();
+    // WASD support
+    this.keys = scene.input.keyboard!.addKeys({
+      w: Phaser.Input.Keyboard.KeyCodes.W,
+      a: Phaser.Input.Keyboard.KeyCodes.A,
+      s: Phaser.Input.Keyboard.KeyCodes.S,
+      d: Phaser.Input.Keyboard.KeyCodes.D,
+    }) as {[key: string]: Phaser.Input.Keyboard.Key};
   }
 
   update(isFishing: boolean) {
-    if (isFishing) return;
+    if (isFishing) {
+      this.sprite.setVelocity(0);
+      return;
+    }
+    let vx = 0, vy=0;
 
     this.sprite.setVelocity(0);
-    if (this.cursors.left?.isDown)  this.sprite.setVelocityX(-SPEED);
-    if (this.cursors.right?.isDown) this.sprite.setVelocityX(SPEED);
-    if (this.cursors.up?.isDown)    this.sprite.setVelocityY(-SPEED);
-    if (this.cursors.down?.isDown)  this.sprite.setVelocityY(SPEED);
+    if (this.cursors.left?.isDown)  vx -= 1;
+    if (this.cursors.right?.isDown) vx += 1;
+    if (this.cursors.up?.isDown)    vy -= 1;
+    if (this.cursors.down?.isDown)  vy += 1;
+    // Diagonal movement is normazlied, not to be faster than vertical and horizontal movement
+    if (vx !==0 && vy !==0) {
+      vx *= Math.SQRT1_2;
+      vy *= Math.SQRT1_2;
+    }
+    this.sprite.setVelocity(vx * SPEED * this.speedMultiplier, vy * SPEED * this.speedMultiplier)
   }
 }
