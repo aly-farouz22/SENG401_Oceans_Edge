6f25d5074ab7fd3d3847128cfed216fe55983308
aasha25 <afsana.asha@ucalgary.ca>
2026-03-17 16:18:12 -0600

    added features and fixes for better handling

--- server/src/controllers/gameController.ts
+++ server/src/controllers/gameController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 import { saveGameState, loadGameState } from "../services/gameService";
 
-export const saveGame = async (req: Request, res: Response) => {
+export const saveGame = async (
+  req: Request, res: Response) => {
   const data = req.body;
   const result = await saveGameState(data);
   res.json(result);
 };
 
-export const loadGame = async (req: Request, res: Response) => {
+export const loadGame = async (
+  req: Request<{ playerId: string }>,
+  res: Response) => {
   const { playerId } = req.params;
   const result = await loadGameState(playerId);
   res.json(result);
